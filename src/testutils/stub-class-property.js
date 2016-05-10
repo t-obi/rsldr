880b1cda7191d25510bdff5258580c660a1a6f58
Tobias Kloht <tobias.kloht@gmail.com>
2016-05-11 01:27:13 +0200

    fix lint-errors in stub-class-property

--- src/testutils/stub-class-property.js
+++ src/testutils/stub-class-property.js
@@ -1,10 +1,9 @@
-const stub = (ClassWithoutStub, propertyName, stub) => {
-  return class ClassWithStub extends ClassWithoutStub {
+const getStub = (ClassWithoutStub, propertyName, stub) =>
+  class ClassWithStub extends ClassWithoutStub {
     constructor(props) {
       super(props);
       this[propertyName] = stub;
     }
   };
-};
 
-export default stub;
+export default getStub;
